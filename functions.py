86f2aa65d59b45d7336158412666f1f19a00a22e
Shruti Chauhan <shrutichauhan.dev@gmail.com>
2025-12-24 13:39:18 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,34 @@
+#Function to sum of all numbers in list
+def sum_of_numbers(list):    
+    sum = 0  
+    for num in list:
+        sum += num
+    return sum
+
+#Function to count vowels in strings
+def count_vowels(str):
+    new_str = str.lower()   #Change string in lowercase and store in new variable, because string is immutable
+    vowel_count = 0
+    for ch in new_str:
+        if ch in "aeiou":
+            vowel_count += 1
+    return vowel_count
+
+#Function to check whether the number is positive, negative or zero
+def check_number(num):
+    if num > 0:
+        return "Positive"
+    elif num < 0:
+        return "Negative"
+    else:
+        return "Zero"
+    
+#main function
+if __name__ == "__main__":
+    nums = [2,4,6,8,10]
+    name = "Shruti Chauhan"
+    digit = 54
+
+    print(f"Sum of all numbers in list is {sum_of_numbers(nums)}")
+    print(f"Vowel count in {name} is {count_vowels(name)}")
+    print(f"{digit} is {check_number(digit)}")
